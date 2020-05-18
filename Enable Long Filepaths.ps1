0c6097cb149ab5da71ac7d583f3164ba8732a7f8
Matthew Lonis <mlonis@ford.com>
2020-05-18 09:33:58 -0400

    Update Enable Long Filepaths.ps1

--- Enable Long Filepaths.ps1
+++ Enable Long Filepaths.ps1
@@ -7,10 +7,10 @@ $Registry_Value = "1"
 if (!(Test-Path $Registry_Path)) {
     New-Item -Path $Registry_Path -Force | Out-Null
 
-    New-ItemProperty -Path $Registry_Path -Name $Registry_Key -Value $Registry_Value -PropertyType DWORD -Force | Out-Null
+    New-ItemProperty -Path $Registry_Path -Name $Registry_Key -Value $Registry_Value -PropertyType DWORD -Force | Write-Host
 } else {
     Write-Host "Updating Registry Value..."
-    New-ItemProperty -Path $Registry_Path -Name $Registry_Key -Value $Registry_Value -PropertyType DWORD -Force | Out-Null
+    New-ItemProperty -Path $Registry_Path -Name $Registry_Key -Value $Registry_Value -PropertyType DWORD -Force | Write-Host
 }
 
 Write-Host "Finished!"

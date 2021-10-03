10330673e455cf99024b715e5c0328ca72588178
Matthew Lonis <mattlonis@gmail.com>
2021-10-03 16:34:09 -0400

    Update system-repair.bat

--- system-repair.bat
+++ system-repair.bat
@@ -1,4 +1,4 @@
-dism /online /cleanup-image /restorehealth && sfc /scannow
+sfc /scannow && dism /online /cleanup-image /restorehealth && sfc /scannow
 
 set /P exit_value="Exit? (Y/N): "
 echo You entered: %exit_value%...

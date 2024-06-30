05e5ac4b165fa67b816ed8194e9c61aa70c54dad
Matthew Lonis <mattlonis@gmail.com>
2024-06-29 23:50:32 -0400

    Create github_desktop_fix.ps1

---
+++ github_desktop_fix.ps1
@@ -0,0 +1,13 @@
+if ($env:Path -notlike "*C:\Program Files\Git\usr\bin*") {
+    Write-Output "C:\Program Files\Git\usr\bin not in PATH"
+    $env:Path += ';C:\Program Files\Git\usr\bin'
+    if ($env:Path -notlike "*C:\Program Files\Git\usr\bin*") {
+        Write-Output "Failed to add C:\Program Files\Git\usr\bin to PATH"
+    }
+    else {
+        Write-Output "Successfully added C:\Program Files\Git\usr\bin to PATH"
+    }
+}
+else {
+    Write-Output "C:\Program Files\Git\usr\bin already in PATH"
+}
